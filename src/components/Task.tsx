8f49668357acbcd2733e1bbfdd3c07b95d019a42
karin knutsson <karinmiriam@gmail.com>
2022-05-17 11:41:55 +0200

    added fontawesome & trash icon

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Todo } from '../model';
 import './styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
 
 const Task: React.FC = () => {
   return (
     <div className="single-task">
+      <FontAwesomeIcon icon={faTrashCan} />
+
     </div>
   );
 };
